cbb34965dd179aad234ec56193b1a5e835704731
Pierre Pelletier <pierre.pelletier@epita.fr>
2021-02-27 23:06:52 +0000

    Disabled host check if using IPV6 THIS SHOULD BE A TEMPORARY MESURE

--- src/vhost/dispatcher.cc
+++ src/vhost/dispatcher.cc
@@ -20,6 +20,7 @@ namespace http
         std::string host = request.host;
         if (host[0] == '[')
         {
+            return;
             host.erase(host.begin());
             host.erase(host.find(']'));
         }
